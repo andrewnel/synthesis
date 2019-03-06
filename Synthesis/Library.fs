0cc9b1243c352c6f0bdff514aa28c33cc98dcb07
andrewnel <andrew.nel.an@gmail.com>
2019-03-06 23:34:37 +0200

    5 functions left

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,37 +1,70 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar x = 
+    x > 12 && x < 3097 && x % 12 = 0
 
-let area _ _ =
-    failwith "Not implemented"
+let area _base _height =
+    match _base < 0.0 || _height < 0.0 with
+    | false -> 0.5*_base*_height 
+    | true -> failwith "Exception: negative height or width not allowed"
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo x =
+    match x < 0 with
+    | true -> -1 * x
+    | _ -> 2 * x
 
-let min _ _ =
-    failwith "Not implemented"
+let min a b =
+    match a < b with
+    | true -> a
+    | _ -> b
 
-let max _ _ =
-    failwith "Not implemented"
+let max a b =
+    match a < b with
+    | true -> b
+    | _ -> a
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime h m s = 3600*h + 60*m + s 
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime s =
+    match s < 0 with
+    | true -> (0, 0, 0)
+    | _ -> (s/3600, (s%3600)/60, (s%3600)%60)
 
-let digits _ =
-    failwith "Not implemented"
+let digits n =
+    let rec count c =
+        match c/10 = 0 with
+        | true -> 1
+        | _ -> 1 + count(c/10)
+    count n
+    
 
 let minmax _ =
     failwith "Not implemented"
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap y =
+    match y < 1582 with
+    | true -> failwith "Exception: invalid year."
+    | _ -> match y % 4 = 0 && y % 100 = 0 with
+           | true -> y % 400 = 0
+           | _ -> y % 4 = 0
+        
 
-let month _ =
-    failwith "Not implemented"
+
+let month = function
+| 1 -> ("January", 31)
+| 2 -> ("February", 28)
+| 3 -> ("March", 31)
+| 4 -> ("April", 30)
+| 5 -> ("May", 31)
+| 6 -> ("June", 30)
+| 7 -> ("July", 31)
+| 8 -> ("August", 31)
+| 9 -> ("September", 30)
+| 10 -> ("October", 31)
+| 11 -> ("November", 30)
+| 12 -> ("December", 31)
+| _ -> failwith "Ëxception: month not supported"
+
 
 let toBinary _ =
     failwith "Not implemented"
