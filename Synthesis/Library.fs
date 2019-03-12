5c19d6052178667ede07e00780efd81f9227606e
andrewnel <andrew.nel.an@gmail.com>
2019-03-12 11:00:37 +0200

    Final commit: All tests pass and within line constraints

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,36 +1,36 @@
 ﻿module Synthesis
 
-let abelar x = 
-    x > 12 && x < 3097 && x % 12 = 0
+let abelar x = // 1 line
+    x > 12 && x < 3097 && x % 12 = 0 
 
-let area _base _height =
+let area _base _height = // 3 lines
     match _base < 0.0 || _height < 0.0 with
     | false -> 0.5*_base*_height 
     | true -> failwith "Exception: negative height or width not allowed"
 
-let zollo x =
+let zollo x = // 3 lines
     match x < 0 with
     | true -> -1 * x
     | _ -> 2 * x
 
-let min a b =
+let min a b = // 3 lines
     match a < b with
     | true -> a
     | _ -> b
 
-let max a b =
+let max a b = // 3 lines
     match a < b with
     | true -> b
     | _ -> a
 
-let ofTime h m s = 3600*h + 60*m + s 
+let ofTime h m s = 3600*h + 60*m + s // 1 line 
 
-let toTime s =
+let toTime s = // 3 lines
     match s < 0 with
     | true -> (0, 0, 0)
     | _ -> (s/3600, (s%3600)/60, (s%3600)%60)
 
-let digits n =
+let digits n = // 5 lines
     let rec count c =
         match c/10 = 0 with
         | true -> 1
@@ -38,12 +38,12 @@ let digits n =
     count n
     
 
-let minmax tuple =
+let minmax tuple = // 3 lines
     match  tuple with
     | (a, b, c, d) -> 
         (min (min a b) (min c d), max (max a b) (max c d)) 
 
-let isLeap y =
+let isLeap y = // 5 lines
     match y < 1582 with
     | true -> failwith "Exception: invalid year."
     | _ -> match y % 4 = 0 && y % 100 = 0 with
@@ -52,7 +52,7 @@ let isLeap y =
         
 
 
-let month = function
+let month = function // 13 lines
 | 1 -> ("January", 31)
 | 2 -> ("February", 28)
 | 3 -> ("March", 31)
@@ -68,7 +68,7 @@ let month = function
 | _ -> failwith "Exception: month not supported"
 
 
-let toBinary i =
+let toBinary i = // 11 lines
     let rec divide2 n =
         match n = 0 with 
         | true -> ""
@@ -81,7 +81,7 @@ let toBinary i =
     | (true, true) -> "0" 
     | _ -> failwith "Exception: expecting positive value"
 
-
+//10 lines
 let bizFuzz n = let rec divisible (i, a, b, c) = match i = n + 1 with | true -> (a,b,c) | false  ->  match (i % 3 = 0, i % 5 = 0, i % 3 = 0 && i % 5 = 0)  with
                                                                                                      | false, false, false -> divisible(i+1,a, b, c)
                                                                                                      | false, false, true -> divisible(i+1,a, b, c+1)
@@ -94,7 +94,7 @@ let bizFuzz n = let rec divisible (i, a, b, c) = match i = n + 1 with | true -> 
                 match n > 0 with | true -> divisible (1,0,0,0) | false -> (0,0,0)
 
 
-let monthDay d y =
+let monthDay d y = // 12 lines
     let rec sumDays i s =
         match s >= d with 
         | true -> i-1
@@ -121,9 +121,9 @@ let sqrt n =
     | _ ->
         calculate (n/2.0) 0
 
-let coord x y =
-    let dist p1 p2 =
-        match p1,p2 with | ((a,b),(c,d)) -> sqrt ((a-c)*(a-c) + (b-d)*(b-d))
-    let within i lc w h =
-        match i,lc with | ((ic1,ic2), (lc1,lc2)) -> (ic1 : float) >= (lc1 : float) && (ic1 : float) <= (lc1+w : float) && (ic2 : float) <= (lc2 : float) && (ic2 : float) >= (lc2-h : float)
-    (dist x y), (fun x y -> within x y)
+let coord x = // 5 lines
+    let dist p2 =
+        match x,p2 with | ((a,b),(c,d)) -> sqrt ((a-c)*(a-c) + (b-d)*(b-d))
+    let within lc w h =
+        match x,lc with | ((ic1,ic2), (lc1,lc2)) -> (ic1 : float) >= (lc1 : float) && (ic1 : float) <= (lc1+w : float) && (ic2 : float) <= (lc2 : float) && (ic2 : float) >= (lc2-h : float)
+    (dist, within)
